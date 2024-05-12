31ae4f4c246ad8af7304fa0114a6f5ccd24c5594
Hearlvein <james.chaintron@gmail.com>
2024-05-12 18:08:04 +0200

    adding packages to module graph

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,7 @@ module org.example.projet_java_2024 {
 
 
     opens org.example.projet_java_2024 to javafx.fxml;
+    opens org.example.projet_java_2024.frontend to javafx.fxml;
+    opens org.example.projet_java_2024.backend to javafx.fxml;
     exports org.example.projet_java_2024;
 }
