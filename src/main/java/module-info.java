745925457da52c447653d353986c2b9de7b9c99c
Hearlvein <james.chaintron@gmail.com>
2024-05-12 16:59:31 +0200

    lombok in module file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.projet_java_2024 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens org.example.projet_java_2024 to javafx.fxml;
